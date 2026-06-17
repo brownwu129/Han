248d70c84fc8f40241e67b960b09c5f64e7fce54
brownwu129 <brownwu129@gmail.com>
2026-06-17 10:43:35 +0800

    Add ros2_ws folder

---
+++ build/russell/CMakeFiles/russell_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/russell_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/russell_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
